1c1e9a9091bce5d3dc9be050bb3cedb5211d2096
mpolishchuk <mmpolishchuk@gmail.com>
2019-11-04 13:34:02 +0200

    homeWork_03

---
+++ Homework_3.Delegate.04.11/Country.Task2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_3.Delegate._04._11
+{
+    delegate void CountryDelegates();
+    public static class Country
+    {
+        public static void ShowPopulation()
+        {
+            Console.WriteLine("\tThe population of the Great Braitain is 66 million inhabitats.");
+        }
+        public static void ShowCapital()
+        {
+            Console.WriteLine("\tLondon is the capital of Great Britain :) ");
+        }
+        public static void ShowQueen()
+        {
+            Console.WriteLine("\tElizabeth 2 - Queen of Great Britain.");
+        }
+
+        public static void DelegatesCombined()
+        {
+            CountryDelegates coutryPopulation = ShowPopulation;
+            CountryDelegates countryCapital = ShowCapital;
+            CountryDelegates countryQueen = ShowQueen;
+            CountryDelegates countryInfo = coutryPopulation + countryCapital + countryQueen;
+
+            Console.WriteLine("1+2+3");
+            countryInfo();
+
+            Console.WriteLine("1+2");
+            CountryDelegates countryInfoOneTwo = coutryPopulation + countryCapital;
+            countryInfoOneTwo();
+
+            Console.WriteLine("1+2, another method");
+            countryInfoOneTwo = countryInfo - countryQueen;
+            countryInfoOneTwo();
+
+            Console.WriteLine("1+2, and try to subtract 3");
+            countryInfo = countryInfoOneTwo - countryQueen;
+            countryInfo();
+
+            Console.WriteLine("(1+3) and (1+2");
+            CountryDelegates countryInfoOneThree = coutryPopulation + countryQueen;
+            CountryDelegates countryInfoCombine = countryInfoOneThree + countryInfoOneTwo;
+            countryInfoCombine();
+        }
+
+    }
+}
