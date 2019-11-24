e80008872ca9b7e1f8abd1fd13a14236ecf82a58
mpolishchuk <mmpolishchuk@gmail.com>
2019-11-24 22:34:49 +0200

    homeWork_08
    Co-Authored-By: Михайло Поліщук <mmmpolishchuk@users.noreply.github.com>

---
+++ Homework_8.EntityFramework.25.11/BookShelfContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using EntityFrameworkLesson.Models;
+
+namespace Homework_8.EntityFramework._25._11
+{
+    public class BookShelfContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Homework_8.EntityFramework.25.11/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework_8.EntityFramework._25._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookShelfContext = new BookShelfService();
+            //bookShelfContext.AddSomeCharacters();
+            bookShelfContext.GetCharactersFromDb();
+           
+            
+        }
+    }
+}
