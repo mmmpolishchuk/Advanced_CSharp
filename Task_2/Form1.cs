676b340f1da5bc2f539d8f74a7460767a803de1a
mpolishchuk <mmpolishchuk@gmail.com>
2019-12-22 14:31:52 +0200

    add_task_11Lesson

---
+++ Task_2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable table = new DataTable();
+        static string connection = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+     
+        public async Task<DataTable> GetDataAsync(string command, string connection)
+        {
+            using (SqlDataAdapter dtAdapter = new SqlDataAdapter(command, connection))
+            {
+                await Task.Run(() => dtAdapter.Fill(table));
+            }
+
+            return table;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string command = @"SELECT * FROM Characters";
+
+            table = await GetDataAsync(command, connection);
+            dataGridView1.DataSource = table;
+        }
+    }
+}
