7ac5b7a9ad5256198a16958bcb0571ec73d580c4
mpolishchuk <mmpolishchuk@gmail.com>
2019-12-22 14:17:01 +0200

    homeWork_11(modified)

--- Homework_11.Threads.04.12/Task_4.cs
+++ Homework_11.Threads.04.12/Task_4.cs
@@ -10,11 +10,14 @@ namespace Homework_11.Threads._04._12
 {
     public static class Task_4
     {
+        public delegate void myDelegate();
         public static void AsyncMethod()
         {
-            Action myDelegate = new Action(CreateText);
+            myDelegate asyncDel = CreateText;
+            asyncDel.BeginInvoke(null, null);
 
-            myDelegate.BeginInvoke(null, null);
+            //Action myDelegate = new Action(CreateText);
+            //myDelegate.BeginInvoke(null, null);
 
             for (int i = 0; i < 100; i++)
             {
@@ -22,13 +25,13 @@ namespace Homework_11.Threads._04._12
                 Console.Write("*");
             }
         }
-        private static void CreateText()
+        private async static void CreateText()
         {
             var file = new FileInfo(@"D:\Homework\Text.txt");
 
             using (StreamWriter writer = file.CreateText())
             {
-                writer.WriteLine("Simple text".ToUpper());
+                await writer.WriteLineAsync("Simple text".ToUpper());
                 Console.WriteLine("Text is written to the file");
             }
         }
